114a488987f12477a605e797fd29da7457a66f61
Eline31 <eline.chambon@gmail.com>
2023-04-24 15:05:58 +0200

    React-router installed - first routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home'
+import About from './pages/About'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
 
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,12 @@
+import Image from '../../assets/About.png'
+
+function About() {
+  return (
+    <div>
+      <img src={Image} alt="Image de montage" />
+      <div>Content</div>
+    </div>
+  )
+}
+
+export default About
