81b49abbb10f4088a97fc98ed50604862f2a5b08
Eline31 <eline.chambon@gmail.com>
2023-05-02 07:18:54 +0200

    Carrousel OK

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -9,6 +9,9 @@ const CarrouselContainer = styled.div`
   height: 410px;
   margin: 0px 100px;
   border-radius: 25px;
+  padding: 10px;
+  background-position: center;
+  background-size: cover;
   button {
     background: none;
     border: none;
@@ -17,47 +20,21 @@ const CarrouselContainer = styled.div`
     position: absolute;
     &:first-child {
       left: 20px;
-      padding-left: 10px;
     }
     &:last-child {
       right: 20px;
-      padding-right: 10px;
     }
   }
   p {
     align-self: flex-end;
     color: white;
-    font-size: 18px;
+    font-size: 24px;
     position: absolute;
     left: 50%;
   }
 `
 
-function BtnSlide({ direction, moveSlide }) {
-  return (
-    <button OnClick={moveSlide}>
-      <img
-        src={direction === 'next' ? arrowright : arrowleft}
-        alt={
-          direction === 'next'
-            ? 'Flèche indiquant la droite'
-            : 'Flèche indiquant la gauche'
-        }
-      />
-    </button>
-  )
-}
-
 function Carrousel({ pictures }) {
-  // const { id } = useParams()
-  // const dataPlace = data.find((place) => {
-  //   if (place.id === id) {
-  //     return { place }
-  //   } else {
-  //     return "Aucun logement correspondant à cet ID n'a été trouvé"
-  //   }
-  // })
-
   const [currentIndex, setCurrentIndex] = useState(0)
 
   const nextSlide = () => {
@@ -79,13 +56,17 @@ function Carrousel({ pictures }) {
       style={{ backgroundImage: `url(${pictures[currentIndex]})` }}
     >
       {pictures.length > 1 && (
-        <BtnSlide direction={'prev'} moveSlide={prevSlide} />
-      )}
-      <p>
-        {currentIndex + 1}/{pictures.length}
-      </p>
-      {pictures.length > 1 && (
-        <BtnSlide moveSlide={nextSlide} direction={'next'} />
+        <>
+          <button alt="Image précédente" onClick={prevSlide}>
+            <img src={arrowleft} alt="Flèche indiquant la gauche" />
+          </button>
+          <p>
+            {currentIndex + 1}/{pictures.length}
+          </p>
+          <button alt="Image suivante" onClick={nextSlide}>
+            <img src={arrowright} alt="Flèche indiquant la droite" />
+          </button>
+        </>
       )}
     </CarrouselContainer>
   )
