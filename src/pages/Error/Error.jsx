3317003de75885d8bf993a9e58ae658f3c64b820
Eline31 <eline.chambon@gmail.com>
2023-05-04 15:37:34 +0200

    Error page responsive OK

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,35 +1,15 @@
-import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-
-const ErrorContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0px 100px;
-  h1 {
-    font-size: 288px;
-    font-weight: 700;
-    margin: 0px;
-  }
-  p:first-of-type {
-    font-size: 36px;
-    margin: 0px 0px 100px 0px;
-  }
-`
-
-const BackHomepage = styled(Link)`
-  font-size: 18px;
-  text-decoration: underline;
-  margin: 100px 0px 50px 0px;
-`
+import './Error.scss'
 
 function Error() {
   return (
-    <ErrorContainer>
+    <div className="ErrorContainer">
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
-      <BackHomepage to="/">Retourner sur la page d'accueil</BackHomepage>
-    </ErrorContainer>
+      <Link className="BackHomepage" to="/">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
   )
 }
 
