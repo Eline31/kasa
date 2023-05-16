b69cdeaa5086f5d99569f95fd25024551fad6509
Eline31 <eline.chambon@gmail.com>
2023-05-16 17:40:09 +0200

    Carousel OK but glitches

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,6 +1,6 @@
 import arrowleft from '../../assets/arrow_left.png'
 import arrowright from '../../assets/arrow_right.png'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './Carrousel.scss'
 import { Loader } from '../../utils/style/Atoms.jsx'
 
@@ -8,67 +8,104 @@ function Carrousel({ pictures }) {
   const [current, setCurrent] = useState(0)
   const [imageSlider, setImageSlider] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const prevDiv = useRef(null)
+  const currentDiv = useRef(null)
+  const nextDiv = useRef(null)
 
-  // useEffect(() => {
-  //   pictures ? setImageSlider(pictures) : setIsLoading(true)
-  //   console.log(imageSlider)
-  // }, [])
   useEffect(() => {
     pictures ? setImageSlider(pictures) : setIsLoading(true)
   }, [])
 
   const nextSlide = () => {
-    if (current < imageSlider.length - 1) {
-      setCurrent(current + 1)
-    } else if (current === imageSlider.length - 1) {
-      setCurrent(0)
+    // Ici on vérifie que les références ont bien été initialisées
+    if (!currentDiv.current && !nextDiv.current) {
+      return
     }
+    currentDiv.current.classList.add('slide-next')
+    nextDiv.current.classList.add('slide-next')
+
+    setTimeout(() => {
+      if (current < imageSlider.length - 1) {
+        setCurrent(current + 1)
+      } else if (current === imageSlider.length - 1) {
+        setCurrent(0)
+      }
+      currentDiv.current.classList.remove('slide-next')
+      nextDiv.current.classList.remove('slide-next')
+    }, 500)
   }
+
   const prevSlide = () => {
-    if (current > 0) {
-      setCurrent(current - 1)
-    } else if (current === 0) {
-      setCurrent(imageSlider.length - 1)
+    if (!prevDiv.current && !currentDiv.current) {
+      return
     }
+    prevDiv.current.classList.add('slide-prev')
+    currentDiv.current.classList.add('slide-prev')
+
+    setTimeout(() => {
+      if (current > 0) {
+        setCurrent(current - 1)
+      } else if (current === 0) {
+        setCurrent(imageSlider.length - 1)
+      }
+      prevDiv.current.classList.remove('slide-prev')
+      currentDiv.current.classList.remove('slide-prev')
+    }, 500)
   }
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     imageSlider[currentIndex]
-  //   }, 5000)
-  // }, [prevSlide, nextSlide])
+  const getPrev = () => {
+    if (current === 0) {
+      return imageSlider.length - 1
+    }
 
-  // console.log(imageSlider)
-  //useEffect sur currentIndex ou useRef
-  // useEffect(() => {
-  //   return <img src={imageSlider[currentIndex]} alt="Carrousel d'images" />
-  // }, [imageSlider[currentIndex]])
-  //Appeler les images ici
+    return current - 1
+  }
+
+  const getNext = () => {
+    if (current === imageSlider.length - 1) {
+      return 0
+    }
+
+    return current + 1
+  }
 
   return (
-    <div className="CarrouselContainer">
-      <>
-        {isLoading ? (
-          <div className="LoaderWrapper">
-            <Loader />
-          </div>
-        ) : (
+    <>
+      {isLoading ? (
+        <div className="LoaderWrapper">
+          <Loader />
+        </div>
+      ) : (
+        <div className="CarrouselContainer">
           <>
-            {imageSlider &&
-              imageSlider.map((image, index) => (
+            {imageSlider && (
+              <>
                 <div
-                  key={index}
-                  className={
-                    index === current
-                      ? 'imgContainer imageOnDisplay'
-                      : 'imgContainer imageOffDisplay'
-                  }
-                >
-                  {index === current && (
-                    <img src={image} alt="Carrousel d'images" />
-                  )}
-                </div>
-              ))}
+                  ref={prevDiv}
+                  className="imgContainer"
+                  style={{
+                    left: `-100%`,
+                    backgroundImage: `url(${imageSlider[getPrev()]})`,
+                  }}
+                />
+                <div
+                  ref={currentDiv}
+                  className="imgContainer"
+                  style={{
+                    left: `0`,
+                    backgroundImage: `url(${imageSlider[current]})`,
+                  }}
+                />
+                <div
+                  ref={nextDiv}
+                  className="imgContainer"
+                  style={{
+                    left: `100%`,
+                    backgroundImage: `url(${imageSlider[getNext()]})`,
+                  }}
+                />
+              </>
+            )}
             {imageSlider.length > 1 && (
               <>
                 <div className="Arrows">
@@ -93,9 +130,9 @@ function Carrousel({ pictures }) {
               </>
             )}
           </>
-        )}
-      </>
-    </div>
+        </div>
+      )}
+    </>
   )
 }
 
