3cb114a7049844c0ba784698a5c4ff51d3882af1
Eline31 <eline.chambon@gmail.com>
2023-05-24 15:44:41 +0200

    Test errorElement

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  // BrowserRouter,
+  // Router,
+  // Routes,
+  // Route,
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx'
 import Accomodation from './pages/Accomodation/Accomodation.jsx'
