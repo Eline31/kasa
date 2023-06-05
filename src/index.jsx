2099262f4632ad0f50152a2a16bf87ac079153e4
Eline31 <eline.chambon@gmail.com>
2023-06-05 10:18:19 +0200

    Router initial back OK

--- src/index.jsx
+++ src/index.jsx
@@ -1,54 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx'
 import Accomodation from './pages/Accomodation/Accomodation.jsx'
 import Error from './pages/Error/Error.jsx'
-import Layout from './components/Layout/Layout.jsx'
-
-const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: (
-        <Layout>
-          <Home />
-        </Layout>
-      ),
-    },
-    {
-      path: '/',
-      children: [
-        {
-          path: 'about',
-          element: (
-            <Layout>
-              <About />
-            </Layout>
-          ),
-        },
-        {
-          path: 'accomodation/:id',
-          element: (
-            <Layout>
-              <Accomodation />
-            </Layout>
-          ),
-        },
-      ],
-    },
-    {
-      path: '*',
-      element: <Error />,
-    },
-  ])
-  return <RouterProvider router={router} />
-}
+import Header from './components/Layout/Header/Header.jsx'
+import Footer from './components/Layout/Footer/Footer.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accomodation/:id" element={<Accomodation />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 )
